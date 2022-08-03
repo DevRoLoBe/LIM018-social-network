1f09730006b187b1012b1095ae44dbe418092ec0
Anais <avalverdea54@gmail.com>
2022-08-03 11:33:33 -0500

    login y registro estilos listos

--- src/componente/login.js
+++ src/componente/login.js
@@ -1,38 +1,32 @@
 export const loginview = () => {
-  const login = /*html*/
-  `
-  <div class="form">
-    <div class="form-login">
-      <figure class="logo">
-        <img class="logo__login" src="imagenes/loginCompleto.png" class="logo hidden" alt="perro y gato abrazadose">
-      </figure>
-      <form action="#">
-        <div class="campo-entrada">
-          <input type="text" placeholder="Ingresar email" required>
-          <i class="uil uil-envelope icon"></i>
-        </div>
-        <div class="campo-entrada">
-          <input type="password" class="password" placeholder="Ingresar contraseña" required>
-          <i class="uil uil-lock icon"></i>
-          <i class="uil uil-eye-slash showHidePw"></i>
-        </div>
-
-        <div class="check-remenber">
-          <div class="check-content">
-            <input type="checkbox" id="check-content__input">
-            <label for="check-content__input" class="check-content__text">Recordarme</label>
+  const login = /*Html*/
+  `<div class="container">
+    <div class="forms">
+      <div class="form login">
+        <!--<figure class="logo">
+          <img class="logo__login" src="imagenes/loginCompleto.png" class="logo hidden" alt="perro y gato abrazadose">
+        </figure>-->
+        <span class="form-title">Login</span>
+        <form action="#">
+          <div class="campo-entrada">
+            <input type="text" placeholder="Ingresar email" required>
+            <i class="uil uil-envelope icon"></i>
           </div>
-          <a href="#" class="check-content__text">¿Olvidaste tu contraseña?</a>
-        </div>
+          <div class="campo-entrada">
+            <input type="password" class="password" placeholder="Ingresar contraseña" required>
+            <i class="uil uil-lock icon"></i>
+            <i class="uil uil-eye-slash showHidePw"></i>
+          </div>
+            <a href="#" class="text">¿Olvidaste tu contraseña?</a>
+          <div class="campo-entrada campo-entrada__boton">
+            <input type="button" value="Iniciar Sesión">
+          </div>
+        </form>
 
-        <div class="campo-entrada campo-entrada__boton">
-          <input class="form-login__boton" type="button" value="Iniciar Sesión">
+        <div class="login-registrar">
+          <span class="text">¿No tiene una cuenta?</span>
+          <a href="#/registro" class="text login-registrar__text--registro">Registrate</a>
         </div>
-      </form>
-
-      <div class="login-registrar">
-        <span clas="login-registrar-text">¿No tiene una cuenta?</span>
-        <a href="#/registro" class="login-registrar__text login-registrar__text--registro">Registrate</a>
       </div>
     </div>
   </div>`;
--- src/componente/registro.js
+++ src/componente/registro.js
@@ -2,42 +2,44 @@ import { register } from '../firebaseconfig/firebase.js';
 
 export const registroview = () => {
   const registro = /*html*/
-  `
-  <div class="form">
-    <div class="form-registro">
-      <figure class="logo">
-        <img class="logo__registro" src="imagenes/titulo.png" class="logo hidden" alt="perro y gato abrazadose">
-      </figure>
-      <form id="form-registro">
-        <div class="campo-entrada">
-          <input type="text" placeholder="Ingresar nombre"  class= "userName" id="userName" required>
-          <i class="uil uil-user"></i>
-          <span id="alertName"></span>
-        </div>
-        <div class="campo-entrada">
-          <input type="email" placeholder="Ingresar email" required  class= "email" id= "userEmail">
-          <i class="uil uil-envelope icon"></i>
-          <span id="alertEmail"></span>
-        </div>
-        <div class="campo-entrada">
-          <input type="password" placeholder="Ingresar contraseña" required class="password" id= "userPassword">
-          <i class="uil uil-lock icon"></i>
-          <span id="alertPassword"></span>
-        </div>
-        <div class="campo-entrada">
-          <input type="password"  placeholder="Confirmar contraseña" required class="confirmarPassword" id= "userConfirmPassword">
-          <i class="uil uil-lock icon"></i>
-          <i class="uil uil-eye-slash showHidePw"></i>
-          <span id="alertConfirmPassword"></span>
-        </div>
-        <div class="campo-entrada campo-entrada__boton">
-          <input id="btn-registrar" class="form-login__boton" type="button" value="Registrarse">
-        </div>
-      </form>
+  `<div class="container">
+    <div class="forms">
+      <div class="form registro">
+        <!--<figure class="logo">
+          <img class="logo__registro" src="imagenes/titulo.png" class="logo hidden" alt="perro y gato abrazadose">
+        </figure>-->
+        <span class="form-title">Registro</span>
+        <form action="#">
+          <div class="campo-entrada">
+            <input type="text" placeholder="Ingresar nombre"  class= "userName" id="userName" required>
+            <i class="uil uil-user"></i>
+            <p id="alertName"></p>
+          </div>
+          <div class="campo-entrada">
+            <input type="email" placeholder="Ingresar email" required  class= "email" id= "userEmail">
+            <i class="uil uil-envelope icon"></i>
+            <span id="alertEmail"></span>
+          </div>
+          <div class="campo-entrada">
+            <input type="password" placeholder="Ingresar contraseña" required class="password" id= "userPassword">
+            <i class="uil uil-lock icon"></i>
+            <span id="alertPassword"></span>
+          </div>
+          <div class="campo-entrada">
+            <input type="password"  placeholder="Confirmar contraseña" required class="confirmarPassword" id= "userConfirmPassword">
+            <i class="uil uil-lock icon"></i>
+            <i class="uil uil-eye-slash showHidePw"></i>
+            <span id="alertConfirmPassword"></span>
+          </div>
+          <div class="campo-entrada campo-entrada__boton">
+            <input id="btn-registrar" type="button" value="Registrarse">
+          </div>
+        </form>
 
-      <div class="login-registrar">
-        <span clas="login-registrar-text">¿Ya tienes una cuenta?</span>
-        <a href="#/login" class="login-registrar__text login-registrar__text--registro">Inicia Sesion</a>
+        <div class="login-registrar">
+          <span class="text">¿Ya tienes una cuenta?</span>
+          <a href="#/login" class="text login-registrar__text--registro">Inicia Sesion</a>
+        </div>
       </div>
     </div>
   </div>`;
@@ -54,20 +56,16 @@ export const registroDom = () => {
   const usernameInput = document.querySelector('#userName').value;
   const emailInput = document.querySelector('#userEmail').value;
   const passwordInput = document.querySelector('#userPassword').value;
+  // const confirmPasswordInput = sectionRegistro.querySelector('.userConfirmPassword').value;
   const alertName = document.querySelector('#alertName');
   // const alertEmail = document.querySelector('#alertEmail');
   btnRegistrar.addEventListener('click', () => {
-    if(usernameInput.length==0){
-   alertName.textContent = 'llena tu nombre';
-   alertName.innerHTML = '';
-   }
-
-   
-    // const confirmPasswordInput = sectionRegistro.querySelector('.userConfirmPassword').value;
+    if (usernameInput.length === 0) {
+      alertName.textContent = 'llena tu nombre';
+    }
     register(emailInput, passwordInput)
       .then((userCredential) => {
         // Signed in
-        console.log(userCredential);
         const user = userCredential.user;
         console.log(user);
         // ...
