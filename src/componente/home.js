1139a65c55a3c6314c737e2a32b66f667b3fded5
Anais <avalverdea54@gmail.com>
2022-08-04 12:04:00 -0500

    iniciando la pantalla principal

--- src/componente/home.js
+++ src/componente/home.js
@@ -1,7 +1,14 @@
 export const homeView = () => {
-  const home = `<h1 class="title">Patitas Preciosas</h1>
+  const home = /*Html*/`
+  <header>
+    <figure class="logo">
+    <img class="logo__registro" src="imagenes/titulo.png" class="logo hidden" alt="perro y gato abrazadose">
+    </figure>
+    <a href ="#"><img src="imagenes/loginAbrazo"></a>
+  </header>
+  <h1 class="title">Patitas Preciosas</h1>
   <div class="cont-logo">
-  <img class="logo" src="imagenes/perroAzul.png" alt="perroAzul">
+  <img class="logo" src="imagenes/titulo.png" alt="patitas preciosas">
   </div>
   <div class="group-btn">
   <a href="#/login"><button class="btn-login btn-general" type="button" id="login">Iniciar sesión</button></a>
